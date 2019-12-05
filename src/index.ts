b4decddcefb43a85cd0d40ae9b4ee264e4ef59ee
avch <webavch@gmail.com>
2019-12-05 16:48:32 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './scss/reset.scss';
+import './scss/base.scss';
+
+import { App } from './app';
+
+const app = new App((<any>window)['external-settings']);
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.ts',
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+		proxy: {
+			'/WebMap/*': {
+				target: 'http://localhost:80',
+				changeOrigin: true,
+				secure: false
+			},
+			'/external-settings.js': { // or create your own file external-settings.js in public folder
+				target: 'http://localhost:80/WebMap/App/ExternalSettings/Tracker-12.js',
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: true
+						},
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	optimization: {
+		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+			ignoreOrder: false,
+		}),
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: './src/index.html',
+			filename: './index.html'
+		})
+	]
+};
